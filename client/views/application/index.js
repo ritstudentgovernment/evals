efc93b6bbc9a07b1518da3882763ea8736d3a01e
petermikitsh <peter.mikitsh@gmail.com>
2015-03-30 17:12:30 -0400

    redesign landing page (#1)

---
+++ client/views/application/index.js
@@ -0,0 +1,16 @@
+Template.index.helpers({
+  tiles: function () {
+    return [{
+      title: "Fast, simple reviews.",
+      description: "Leave as much or as little feedback as you want. Work at your own pace. After reviewing just 2 courses, you will have access to search all evaluation data."
+    },
+    {
+      title: "Choose courses with confidence.",
+      description: "Find the best-rated professors that compliment your learning style."
+    },
+    {
+      title: "Secure and Confidential.",
+      description: "Your personal information is not shared with anyone when writing reviews."
+    }];
+  }
+});
