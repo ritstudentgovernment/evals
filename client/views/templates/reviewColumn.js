e830a7ff3e1b91ba83643669d564e7fe4086fed0
petermikitsh <peter.mikitsh@gmail.com>
2015-04-13 17:59:53 -0400

    "no reviews to show" message; hide empty reviews
    Center summary table view on small viewports

--- client/views/templates/reviewColumn.js
+++ client/views/templates/reviewColumn.js
@@ -1,6 +1,6 @@
 Template.reviewColumn.helpers({
   evaluations: function () {
-    return Evaluations.find().fetch();
+    return Evaluations.find({$or: [{courseComments: {$exists: true}}, {instructorComments: { $exists: true }}]}).fetch();
   },
   evaluationCreatedAt: function () {
     return new moment(this.createdAt).fromNow();
