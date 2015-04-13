5013f949ce92c39824fdd7b0e0ddd244ef1c572f
petermikitsh <peter.mikitsh@gmail.com>
2015-04-12 23:39:49 -0400

    end-to-end global evaluation term in singleton
    The global evaluationTerm is used to verify users have completed at
    least 2 reviews for the current term before being granted access to the
    search evaluation data. This means that users must continually complete
    2 reviews for each term to maintain their querying ability.

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,13 +1,23 @@
 Meteor.startup(function() {
   // Initialize admin user
   if (Meteor.users.find().count() === 0) {
-    var adminUser = Meteor.users.insert({username: "sgweb"});
+    var adminUser = Meteor.users.insert({
+      username: "sgweb",
+      identity: {
+        name: "Peter Mikitsh",
+        firstName: "Peter",
+        lastName: "Mikitsh"
+      },
+      evaluationCounts: [],
+      sectionIds: []
+    });
     Roles.addUsersToRoles(adminUser, ['admin']);
   }
   // Initialize singleton
   if (Singleton.find().count() === 0) {
     Singleton.insert({
       evaluationCount: 0,
+      evaluationTerm: 20145,
       version: "v1.0.0-dev"
     });
   }
