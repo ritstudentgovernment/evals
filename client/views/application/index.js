007e2fc1ea221fdc18dd4f65a1529d1ec566d9e0
petermikitsh <peter.mikitsh@gmail.com>
2015-04-15 11:30:10 -0400

    alternate landing page media icon layout
    Creates more visual balance on landing page.

--- client/views/application/index.js
+++ client/views/application/index.js
@@ -3,17 +3,20 @@ Template.index.helpers({
     return [{
       title: "Fast, simple reviews.",
       description: "Leave as much or as little feedback as you want. Work at your own pace. After reviewing just 2 courses, you will have access to search all evaluation data.",
-      icon: "/images/clock.svg"
+      icon: "/images/clock.svg",
+      mediaClass: "media-left"
     },
     {
       title: "Choose courses with confidence.",
       description: "Find the best-rated professors that compliment your learning style.",
-      icon: "/images/nav.svg"
+      icon: "/images/nav.svg",
+      mediaClass: "media-right"
     },
     {
       title: "Secure and Confidential.",
       description: "Your personal information is not shared with anyone when writing reviews.",
-      icon: "/images/lock.svg"
+      icon: "/images/lock.svg",
+      mediaClass: "media-left"
     }];
   }
 });
