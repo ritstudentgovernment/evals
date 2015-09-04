80e8147fc8cd08bdcea6213d17ee1054adcecd44
James Michael Reilly <jmr8893@rit.edu>
2015-09-04 16:36:19 -0400

    Professor's sections now have their own template to format it better

--- client/views/templates/sectionPanel.js
+++ client/views/templates/sectionPanel.js
@@ -54,15 +54,56 @@ Template.sectionPanel.events({
           if(!session){
             Router.go('evaluationNew', {"sectionId": sectionId});
           }else{
-            $("#" + session).collapse('hide');
+            $("#" + session.id).collapse('hide');
             Session.set('selectedItem', undefined);
           }
         }else{
           throwError(result.message);
         }
       }
-
     });
+  }
+});
 
+
+Template.professorSectionPanel.helpers({
+  sections: function () {
+    var singleton = Singleton.findOne();
+    var selectedItem = Session.get("selectedItem");
+    Meteor.call('sectionsForProfessor',
+      selectedItem.name,
+      Singleton.findOne().evaluationTerm,
+      function (err, result) {
+        Session.set('sectionsForCourse', result);
+      }
+    );
+    return Session.get('sectionsForCourse');
+  },
+});
+
+Template.professorSectionPanel.events({
+  'click .addEvalBtn': function (event, template) {
+    var sectionId = event.currentTarget.id;
+    var sectionNum = event.currentTarget.attributes.courseNum.value;
+    var options = {
+      sectionID: sectionNum
+    }
+    var session = Session.get('selectedItem');
+    Meteor.call('addCourse',options, function (error, result) {
+      if(error){
+
+      }else{
+        if(!result.error){
+          if(!session){
+
+          }else{
+            $("#" + session.id).collapse('hide');
+            Session.set('selectedItem', undefined);
+          }
+        }else{
+          throwError(result.message);
+        }
+      }
+    });
   }
 });
